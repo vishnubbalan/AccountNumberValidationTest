184148b0049f4a1e2d7c9bd81132a458d4a1d628
vb7856 <vb7856@VISHNTRVCMS02>
2022-02-20 15:08:01 +0530

    extentReports

---
+++ AccountNumberValidationTest/Hooks.cs
@@ -0,0 +1,53 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports.Reporter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace AccountNumberValidationTest
+{
+    [Binding]
+    public class Hooks
+    {
+        private static ExtentTest featureName;
+        private static ExtentTest scenario;
+        private static ExtentReports extent;
+
+        [BeforeTestRun]
+        public static void BeforeTestRunHook()
+        {
+            ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(@"C:\Users\VB7856\Desktop\AccountNumerValidation\ExtendReport.html");
+            htmlReporter.Configuration().Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+        }
+
+        [AfterTestRun]
+        public static void TearDownReports()
+        {
+            extent.Flush();
+        }
+
+        [BeforeFeature]
+        public static void BeforeFeature()
+        {
+            featureName = extent.CreateTest<Feature>(FeatureContext.Current.FeatureInfo.Title);
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            scenario = featureName.CreateNode<Scenario>(ScenarioContext.Current.ScenarioInfo.Title);
+        }
+
+        [AfterStep]
+        public void InsertReportingSteps()
+        {
+            scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
+        }
+    }
+}
