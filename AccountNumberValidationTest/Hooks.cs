9bb276440753f018009c8c2b7045d2697d5e64af
vb7856 <vb7856@VISHNTRVCMS02>
2022-02-20 15:34:44 +0530

    extenr reports removed

--- AccountNumberValidationTest/Hooks.cs
+++ AccountNumberValidationTest/Hooks.cs
@@ -1,7 +1,4 @@
-﻿using AventStack.ExtentReports;
-using AventStack.ExtentReports.Gherkin.Model;
-using AventStack.ExtentReports.Reporter;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,60 +10,60 @@ namespace AccountNumberValidationTest
     [Binding]
     public class Hooks
     {
-        private static ExtentTest featureName;
-        private static ExtentTest scenario;
-        private static ExtentReports extent;
+        //private static ExtentTest featureName;
+        //private static ExtentTest scenario;
+        //private static ExtentReports extent;
 
-        [BeforeTestRun]
-        public static void BeforeTestRunHook()
-        {
-            ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(@"C:\Users\VB7856\Desktop\AccountNumerValidation\ExtendReport.html");
-            htmlReporter.Configuration().Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
-            extent = new ExtentReports();
-            extent.AttachReporter(htmlReporter);
-        }
+        //[BeforeTestRun]
+        //public static void BeforeTestRunHook()
+        //{
+        //    ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(@"C:\Users\VB7856\Desktop\AccountNumerValidation\ExtendReport.html");
+        //    htmlReporter.Configuration().Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+        //    extent = new ExtentReports();
+        //    extent.AttachReporter(htmlReporter);
+        //}
 
-        [AfterTestRun]
-        public static void TearDownReports()
-        {
-            extent.Flush();
-        }
+        //[AfterTestRun]
+        //public static void TearDownReports()
+        //{
+        //    extent.Flush();
+        //}
 
-        [BeforeFeature]
-        public static void BeforeFeature()
-        {
-            featureName = extent.CreateTest<Feature>(FeatureContext.Current.FeatureInfo.Title);
-        }
+        //[BeforeFeature]
+        //public static void BeforeFeature()
+        //{
+        //    featureName = extent.CreateTest<Feature>(FeatureContext.Current.FeatureInfo.Title);
+        //}
 
-        [BeforeScenario]
-        public void BeforeScenario()
-        {
-            scenario = featureName.CreateNode<Scenario>(ScenarioContext.Current.ScenarioInfo.Title);
-        }
+        //[BeforeScenario]
+        //public void BeforeScenario()
+        //{
+        //    scenario = featureName.CreateNode<Scenario>(ScenarioContext.Current.ScenarioInfo.Title);
+        //}
 
-        [AfterStep]
-        public void InsertReportingSteps()
-        {
-            var stepType = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
-            if(ScenarioContext.Current.TestError == null)
-            {
-                if (stepType == "Given")
-                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
-                else if (stepType == "When")
-                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
-                else if (stepType == "Then")
-                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
-            }
-            else if(ScenarioContext.Current.TestError != null)
-            {
-                if (stepType == "Given")
-                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.InnerException.Message);
-                else if (stepType == "When")
-                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.InnerException.Message);
-                else if (stepType == "Then")
-                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.InnerException.Message);
-            }
+        //[AfterStep]
+        //public void InsertReportingSteps()
+        //{
+        //    var stepType = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
+        //    if(ScenarioContext.Current.TestError == null)
+        //    {
+        //        if (stepType == "Given")
+        //            scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
+        //        else if (stepType == "When")
+        //            scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
+        //        else if (stepType == "Then")
+        //            scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
+        //    }
+        //    else if(ScenarioContext.Current.TestError != null)
+        //    {
+        //        if (stepType == "Given")
+        //            scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.InnerException.Message);
+        //        else if (stepType == "When")
+        //            scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.InnerException.Message);
+        //        else if (stepType == "Then")
+        //            scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Fail(ScenarioContext.Current.TestError.InnerException.Message);
+        //    }
             
-        }
+        //}
     }
 }
