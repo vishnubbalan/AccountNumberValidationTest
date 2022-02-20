8c3281e7f24d8b3de3f82681e18033319d6d4e82
vb7856 <vb7856@VISHNTRVCMS02>
2022-02-20 13:18:34 +0530

    mstest updated

---
+++ AccountNumberValidationTest/Specs/ApiEvents.feature
@@ -0,0 +1,9 @@
+﻿Feature: ApiEvents
+	Simple calculator for adding two numbers
+
+@BankAccountNumberVerification
+Scenario: BankAccountVerification
+	Given I have API URL https://api-test.afterpay.dev/api/v3/validate/bank-account
+	And the XAuthKey is Q7DaxRnFls6IpwSW1SQ2FaTFOf7UdReAFNoKY68L
+	When I verify the AccountNumber GB09HAOE91311808002317
+	Then isValid in Responce is True
---
+++ AccountNumberValidationTest/Specs/ApiEventsSteps.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace AccountNumberValidationTest.Specs
+{
+    [Binding]
+    public class ApiEventsSteps
+    {
+        string Endpoint = null;
+        string AccNumber = null;
+        string XAuthKey = null;
+        string isTrue = null;
+        ApiResponce apiResponce;
+
+        [Given(@"I have API URL (.*)")]
+        public void GivenIHaveAPIURL(string url)
+        {
+            Endpoint = url;
+        }
+
+        [Given(@"the XAuthKey is (.*)")]
+        public void GivenTheXAuthKeyIs(string key)
+        {
+            XAuthKey = key;
+        }
+
+        [When(@"I verify the AccountNumber (.*)")]
+        public void WhenIVerifyTheAccountNumber(string AccNumber)
+        {
+            ApiHelper helper = new ApiHelper();
+            apiResponce = helper.CreateRequest(AccNumber, Endpoint, XAuthKey);
+        }
+
+        [Then(@"isValid in Responce is (.*)")]
+        public void ThenIsValidInResponceIs(string isTrue)
+        {
+            if (apiResponce != null)
+                Assert.IsTrue(apiResponce.isValid.ToString().Equals(isTrue), "Mismatch in isValid responce");
+            else
+                Assert.Fail("Exception occured in responce");
+        }
+    }
+}
